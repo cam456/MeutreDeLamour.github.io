1d3b25d34d8e533798b7cd7356bf82f7eb1076d5
Cam <112569387+p000hs@users.noreply.github.com>
2023-09-29 19:16:11 -0400

    yes

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ let chapters = {
         image : "/image/IGA.jpg",
         boutons : [
             { titre : "dire ou vous étier le 21 septembre",  destination: "arrestation" },
-            { titre : "ne pas ment",  destination: "questionnement" }
+            { titre : "ne pas ment",  destination: "VraiAmour" }
         ]  
 
     },
@@ -43,42 +43,57 @@ let chapters = {
         titre : "Le vrai amour" ,
         text : "contenue premier chapitre",
         image : "/image/IGA.jpg",
-        boutons : []  
+        boutons : [
+            { titre : "ne pas crier et demander un advocat ",  destination: "avocat" },
+            { titre : "crier que vous l'aimer et que vous voulez un advocat",  destination: "avocat" }
+    ]  
 
     },
     avocat: {
         titre : "Le sauveur" ,
         text : "contenue premier chapitre",
         image : "/image/IGA.jpg",
-        boutons : []  
+        boutons : [
+            { titre : "vous avez crier au policier",  destination: "innocence" },
+            { titre : "vous avez pas crier au policier",  destination: "innocence" }
+        ]  
 
     },
     innocence: {
         titre : "notre innocence" ,
         text : "contenue premier chapitre",
         image : "/image/IGA.jpg",
-        boutons : []  
+        boutons : [
+            { titre : "innocent",  destination: "arrestation" },
+            { titre : "coupable",  destination: "VraiAmour" }
+        ]  
 
     },
     juge: {
         titre : "Juge" ,
         text : "contenue premier chapitre",
         image : "/image/IGA.jpg",
-        boutons : []  
+        boutons : [
+            { titre : "suivent",  destination: "verdicte" },
+        ]  
 
     },
     verdicte: {
         titre : "VERDICTE" ,
         text : "contenue premier chapitre",
         image : "/image/IGA.jpg",
-        boutons : []  
+        boutons : [
+            { titre : "recommencer",  destination: " debut" },
+        ]  
 
     },
     mauvaisVerdicte: {
         titre : "VERDICTE" ,
         text : "contenue premier chapitre",
         image : "/image/IGA.jpg",
-        boutons : []  
+        boutons : [
+            { titre : "recommencer",  destination: " debut" }
+        ]  
 
     },
 }
@@ -88,6 +103,7 @@ let chapters = {
 function goToChapter(cle){
 let chapitre = chapters[cle];
 console.log (chapitre.titre)
+console.log(chapitre.text)
 // affiche le text
 // boucle a travers les boutons
 // dans la boucle, affiche bouton.destination.
